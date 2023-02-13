f08c64eb3e4759cea6a1cd55c7e2ad619ac0ccf3
Grechko Maria <grechko.maria2003@gmail.com>
2023-02-13 12:19:10 +0300

    finished flags

--- lib/commander.rb
+++ lib/commander.rb
@@ -2,27 +2,36 @@ require_relative './initializator.rb'
 require_relative './removed_files_tracker.rb'
 require_relative './recycle_bin.rb'
 require_relative './printer.rb'
+require 'optparse'
 
 class Commander
   def self.call
+    $printer = Printer.new
     ARGV.include?('--silent') ? $printer = Printer.new(true) : $printer = Printer.new
     ARGV.reject! { |argv| argv == '--silent' }
-    case ARGV[0]
-      when '--setup'
+    OptionParser.new do |parser|
+      parser.on('-i', '--init', 'Initializes recycle bin and files tracker.') do
         Initializator.call
-      when '--file_to_bin'
-        RecycleBin.file_to_bin(ARGV[1..])
-      when '--restore'
-        RecycleBin.restore_from_bin(ARGV[1..])
-      when '--restore_all'
-        RecycleBin.restore_all_from_bin
-      when '--delete_file_from_bin'
-        RecycleBin.delete_file_from_bin(ARGV[1..])
-      when '--clean'
-        RecycleBin.delete_all_from_bin
-      when '--show'
+      end
+      parser.on('-l', '--list', 'Prints recycle bin contents.') do
         RecycleBin.print_bin_contents
-      else puts 'Invalid command'
-    end
+      end
+      parser.on('-d', 'Deletes user input files from current directory.') do
+        RecycleBin.file_to_bin(ARGV)
+      end
+      parser.on('-c', '--clean', 'Permanently deletes all data from the recycle bin.') do
+        RecycleBin.delete_all_from_bin
+      end
+      parser.on("-r", 'Restores user input files from the recycle bin.') do
+        RecycleBin.restore_from_bin(ARGV)
+      end
+      parser.on('--restore_all', 'Restores all data from the recycle bin.') do
+        RecycleBin.restore_all_from_bin
+      end
+      parser.on('-h', '--help') do
+        puts parser
+      end
+    end.parse!
   end
 end
+
